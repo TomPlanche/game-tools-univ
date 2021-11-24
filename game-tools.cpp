da24a8337cd7ad00eaa11d41b365337747547723
Tom Planche <58936594+TomPlanche@users.noreply.github.com>
2021-11-24 14:49:11 +0100

    premer commit

---
+++ game-tools.cpp
@@ -0,0 +1,86 @@
+/**\
+  * @file    game-tools.cpp
+  * @author T.Planche
+  * @brief  Corps du module game-tools
+\**/
+
+#include "game-tools.h"
+
+# ifdef _WIN32
+    #include <conio.h>
+# else
+    #include <unistd.h>
+# endif
+
+using namespace OS;
+
+#define RESET "\033[0m"
+#define ROUGE "\033[0;31m"
+#define VERT "\033[0;32m"
+#define JAUNE "\033[0;33m"
+#define BLEU "\033[0;34m"
+#define VIOLET "\033[0;35m"
+#define CYAN "\033[0;36m"
+#define BLANC "\033[0;37m"
+
+
+#include <chrono>  // pour la fonction now() utilisée dans la fonction random()
+#include <random>  // pour la fonction random
+
+using namespace std::chrono;
+
+
+# ifdef __APPLE__
+    // Retourne le code couleur de la couleur passé en paramètre
+    string getCodeCouleur (Couleur couleur) {
+        string codeCouleur;
+        switch (couleur)
+        {
+        case bleu:
+            codeCouleur = BLEU;
+            break;
+
+        case vert:
+            codeCouleur = VERT;
+            break;
+
+        case cyan:
+            codeCouleur = CYAN;
+            break;
+
+        case rouge:
+            codeCouleur = ROUGE;
+            break;
+
+        case violet:
+            codeCouleur = VIOLET;
+            break;
+
+        case jaune:
+            codeCouleur = JAUNE;
+            break;
+
+        case blanc:
+            codeCouleur = BLANC;
+            break;
+
+        default:
+            codeCouleur = RESET;
+            break;
+        }
+
+        return codeCouleur;
+    }
+# endif
+
+int OS::random(int min, int max) {
+    std::default_random_engine generateur;
+    std::uniform_int_distribution<int> distributionNombres;
+    unsigned int tempsActuel = static_cast<unsigned int>(steady_clock::now().time_since_epoch().count());
+    generateur.seed(tempsActuel);
+
+    return ((distributionNombres(generateur) % (max - min + 1)) + min);
+}
+
+
+
---
+++ game-tools.h
@@ -0,0 +1,116 @@
+/**\
+  * @file    game-tools.h
+  * @author T.Planche
+  * @brief  Fichier d'entête du module game-tools
+\**/
+
+# ifndef GAME_TOOLS_H
+# define GAME_TOOLS_H
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::flush;
+
+
+# ifdef _WIN32
+    #include <windows.h>
+# endif
+
+/**\
+  * @brief Liste de couleurs utilisables avec les procedures #afficherTexteEnCouleur et #afficherNombreEnCouleur
+\**/
+# ifdef _WIN32
+    enum Couleur {
+        /** La couleur gris a pour code couleur 7*/
+        gris = 7,
+        /** La couleur bleu a pour code couleur 9*/
+        bleu = 9,
+        /** La couleur vert a pour code couleur 10*/
+        vert = 10,
+        /** La couleur cyan a pour code couleur 11*/
+        cyan = 11,
+        /** La couleur rouge a pour code couleur 12*/
+        rouge = 12,
+        /** La couleur violet a pour code couleur 13*/
+        violet = 13,
+        /** La couleur jaune a pour code couleur 14*/
+        jaune = 14,
+        /** La couleur blanc a pour code couleur 15*/
+        blanc = 15
+    };
+# else
+    enum Couleur {gris, bleu, vert,  cyan, rouge, violet, jaune, blanc};
+# endif
+
+namespace OS {
+    /**
+     * @brief Permet d'afficher une chaine de caractères en couleur.
+     *
+     * @param [in] chaine la chaine à afficher.
+     * @param [in] couleur la couleur à utiliser pour afficher la chaine. La couleur doit faire partie
+     * des valeurs proposées par le type #Couleur
+     * @param [in] retourALaLigne indique si l'affichage de la chaine doit être suivi d'un
+     * retour à la ligne (true) ou pas (false).
+     */
+    void afficherTexteEnCouleur(string chaine, Couleur couleur, bool retourALaLigne = false);
+
+
+    /**
+     * @brief Permet d'afficher un caractère en couleur.
+     *
+     * @param [in] caractere le caractère à afficher.
+     * @param [in] couleur la couleur à utiliser pour afficher le caractère. La couleur doit faire partie
+     * des valeurs proposées par le type #Couleur
+     * @param [in] retourALaLigne indique si l'affichage du caractère doit être suivi d'un
+     * retour à la ligne (true) ou pas (false).
+     */
+    void afficherTexteEnCouleur(char caractere, Couleur couleur, bool retourALaLigne = false);
+    
+
+    /**
+     * @brief Permet d'afficher un nombre en couleur.
+     *
+     * @param [in] nombre le nombre à afficher. Il peut s'agit d'un nombre entier ou d'un nombre à virgule.
+     * @param [in] couleur la couleur à utiliser pour afficher le nombre. La couleur doit faire partie
+     * des valeurs proposées par le type #Couleur
+     * @param [in] retourALaLigne indique si l'affichage du nombre doit être suivi d'un
+     * retour à la ligne (true) ou pas (false).
+     */
+    void afficherNombreEnCouleur(double nombre, Couleur couleur, bool retourALaLigne = false);
+
+
+    /**
+     * @brief Génère un nombre entier aléatoire compris dans un intervalle donné.
+     *
+     * @param [in] min la borne min de l'intervalle.
+     * @param [in] max la borne max de l'intervalle.
+     * @return int le nombre aléatoire généré.
+     */
+    int random(int min, int max);
+
+
+    /**
+     * @brief Met en pause l'exécution du programme durant un temps
+     * déterminé ou jusqu'à ce que l'utilisateur appuie sur une touche.
+     *
+     * @param [in] dureeEnSecondes la durée en secondes de la pause. Si ce 
+     * paramètre est égal à 0, le programme s'interrompt jusqu'à ce que
+     * l'utilisateur appuie sur une touche.
+     */
+    void pause(unsigned int dureeEnSecondes = 0);
+
+
+    /**
+     * @brief Efface le contenu du terminal
+     *
+     */
+    void effacer();
+
+
+} // namespace OS
+
+
+# endif
